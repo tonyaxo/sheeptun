711deb7ee19dfcf49082c48883fc056a89bdcee3
Sergey Bogatyrev <sergey@bogatyrev.me>
2026-09-13 04:46:42 +0200

    Initial Commit

---
+++ sheeptun/sheeptunApp.swift
@@ -0,0 +1,17 @@
+//
+//  sheeptunApp.swift
+//  sheeptun
+//
+//  Created by Sergey Bogatyrev on 13/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct sheeptunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sheeptunTests/sheeptunTests.swift
@@ -0,0 +1,19 @@
+//
+//  sheeptunTests.swift
+//  sheeptunTests
+//
+//  Created by Sergey Bogatyrev on 13/09/2026.
+//
+
+import Testing
+@testable import sheeptun
+
+struct sheeptunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
